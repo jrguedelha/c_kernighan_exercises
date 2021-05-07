787e6a2cbb49ca1da5b27726cdff18eb4dc569e7
jrguedelha <jrguedelha@gmail.com>
2021-05-07 13:54:31 -0300

    Function Power

---
+++ main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<power.c>
+
+void main()
+{
+	printf("%d\n",power(8,2));
+}
---
+++ power.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/* Calcules power */
+
+void main()
+{
+	printf("%d\n",power(6,2));
+}
+
+int power(int base, int p)
+{
+	int i;
+	int result;
+
+	i = 0;
+	result = 1;
+	while(i < p)
+	{
+		result = result * base;
+		++i;
+	}
+	return result;
+}
