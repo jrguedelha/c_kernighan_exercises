6e2e5575a4f758589fc1b37a9ffb7998d8ab3e04
jrguedelha <jrguedelha@gmail.com>
2021-05-13 11:52:47 -0300

    ex1-17 code

---
+++ ex1-17.c
@@ -0,0 +1,45 @@
+/*get lines and print the lines greater than MAX*/
+#include<stdio.h>
+#define SPAN 100
+#define MAX 20
+
+int get_line_length(char line[],int span);
+
+int main()
+{
+	int length;
+	char line[SPAN];
+
+	length = 0;
+	while((length = get_line_length(line, SPAN)) > 0)
+	{
+		if(length > MAX)
+		{
+			printf("\n%s(%d)\n",line,length);
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	return (0);
+}
+
+
+int get_line_length(char s[], int span)
+{
+	int c;
+	int i;
+
+	i = 0;
+	while((i < span-1) && ((c = getchar()) != EOF) && (c != '\n'))
+	{
+		s[i] = c;
+		++i;
+	}
+	if(c == '\n')
+	{
+		s[i] = '\n';
+	}
+	return (i);
+}
